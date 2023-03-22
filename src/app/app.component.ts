46d3dc0ff3527fa688da9c7191f36e21105b1094
behrooz bozorg chami <behroozbc@outlook.com>
2023-03-22 12:21:46 +0430

    add chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { EChartsOption } from 'echarts';
+import { PieChart } from 'echarts/charts';
+import { LegendComponent, TitleComponent, TooltipComponent } from 'echarts/components';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sample-of-echart';
+export class AppComponent implements OnInit {
+  readonly echartsExtentions;
+  echartsOptions: EChartsOption = {};
+  constructor () {
+    this.echartsExtentions = [PieChart, TitleComponent, TooltipComponent, LegendComponent];
+  }
+  ngOnInit(): void {
+    this.echartsOptions = {
+      title: {
+        text: 'Spending my time',
+        subtext: 'Fake Data',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left'
+      },
+      series: [
+        {
+          name: 'Time',
+          type: 'pie',
+          radius: '50%',
+          data: [
+            { value: 980, name: 'Youtube' },
+            { value: 501, name: 'Writing blog' },
+            { value: 100, name: 'Watch TDD' },
+            { value: 200, name: 'C# source generator' },
+            { value: 250, name: 'Play Valorant' }
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EchartsxModule } from 'echarts-for-angular';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EchartsxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
